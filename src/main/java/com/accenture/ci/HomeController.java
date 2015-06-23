e21ba4a2343dba3b068d37e4fd6bfe07b88c4cd6
shaunak <shaunak.soni@gmail.com>
2015-06-23 18:50:21 +0530

    first commit

--- src/main/java/com/accenture/ci/HomeController.java
+++ src/main/java/com/accenture/ci/HomeController.java
@@ -28,7 +28,6 @@ public class HomeController {
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		DateFormat dateFormat1 = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
